1e2427f60c525e41d8c47ea29ad16205ca2f8dd7
yayaya123456 <s58845566@yahoo.com.tw>
2019-10-25 16:32:39 +0800

    ch699

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def main(request):
     '''
     Show 'Hello world!' in the main page
     '''
-    return HttpResponse('Hello world!')
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
